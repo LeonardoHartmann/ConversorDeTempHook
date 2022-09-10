ca3054dc4f3614dd069747307b82e6600cd9bec1
Leonardo <leonardohartmann03@gmail.com>
2022-09-09 21:05:38 -0300

    ConversorDeSub

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const ConversorDeTemp = () => {
+
+    document.title='ConversorDeTemp'
+
+    const [temperatura, setTemperatura] = useState('') 
+    const [opcao, setOpcao] = useState('')  
+    const [result, setResult] = useState('') 
+    const [exibirResultado, setexibirResultado] = useState('')
+
+
+  const Exibir = () => {
+    if (opcao === 'F'){
+      setResult(((temperatura) * 5)/9)
+      setexibirResultado('Resultado: ' + result + opcao)
+    }
+
+    else if(opcao === "C") {
+      setResult(((temperatura * 9) / 5) + 32)
+      setexibirResultado('Resultado: ' + result + opcao)
+    }
+  }
+
+    return(
+        <div>
+            <h1>Conversor de Temperatura</h1>
+            <br/>
+            
+            Temperatura:<br/> <br/>
+            <label>
+                <input type= 'text'
+                onChange={(event)=> setTemperatura(event.target.value)}/>  
+            </label>
+            <label>
+                <input type='radio' 
+                checked={opcao === 'F'}
+                onChange={()=> setOpcao('F')}/> Fahrenheit(F)
+            </label>
+            <label>
+                <input type='radio' 
+                checked={opcao === 'C'}
+                onChange={()=> setOpcao('C')}/> Celsius(C)
+            </label>
+            <br/><br/>
+            
+            <input type='button' value='Verificar'
+                onClick={()=> Exibir()}/>
+            <br/><br/>
+
+            {exibirResultado}
+
+        </div>
+    )
+
 }
 
-export default App;
+export default ConversorDeTemp
